29430f58ec9c803b489ec7b741f35be280faf668
Ana Laura <165479726+AnafraUnivesp@users.noreply.github.com>
2024-10-10 18:27:58 -0300

    alterando a estrutura

---
+++ projeto_cad/projeto_cad/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from app_cad_clientes import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('clientes/', views.clientes, name='listagem_clientes'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
+    path('excluir/<int:id>/', views.excluir_cliente, name='excluir_cliente'),
+    path('editar_observacao/<int:id_observacao>/', views.editar_observacao, name='editar_observacao'),
+    path('excluir_observacao/<int:id_observacao>/', views.excluir_observacao, name='excluir_observacao'),
+    path('adicionar_observacao/<int:id_cliente>/', views.adicionar_observacao, name='adicionar_observacao'),
+]
